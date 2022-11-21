a4a5536b6c9eef5b0c9e1f5d9a367dbea4ccec03
Kriptikz <Kriptikz@users.noreply.github.com>
2022-11-20 23:27:14 -0500

    Create ShadowDriveApi Service and load state to ViewModel

--- app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreen.kt
+++ app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreen.kt
@@ -1,41 +1,74 @@
 package com.kriptikz.shadowdrivemobile.ui.screens.drive_screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kriptikz.shadowdrivemobile.ui.screens.SDMScaffold
 import com.kriptikz.shadowdrivemobile.ui.theme.ShadowDriveMobileTheme
+import com.kriptikz.shadowdrivemobile.R
 
 @Composable
 fun DriveScreen(
-    driveUiState: String,
+    driveUiState: DriveUiState,
     modifier: Modifier = Modifier
 ) {
     SDMScaffold(title = "Drives") {
-        ResultScreen(driveUiState = driveUiState, modifier = modifier)
+        when (driveUiState) {
+            is DriveUiState.Loading -> LoadingScreen(modifier)
+            is DriveUiState.Success -> ResultScreen(fileNames = driveUiState.fileNames, modifier = modifier)
+            is DriveUiState.Error -> ErrorScreen(modifier)
+        }
     }
 }
 
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(id = R.drawable.loading_img),
+            contentDescription = stringResource(id = R.string.loading)
+        )
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(stringResource(id = R.string.loading_failed))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DriveScreenPreview() {
     ShadowDriveMobileTheme {
-        DriveScreen("Some State")
+        DriveScreen(DriveUiState.Success(listOf("Some State")))
     }
 }
 
 @Composable
-fun ResultScreen(driveUiState: String, modifier: Modifier = Modifier) {
+fun ResultScreen(fileNames: List<String>, modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
     ) {
-      Text(text = driveUiState)
+      Text(text = fileNames.toString())
     }
 
 }
--- app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreenViewModel.kt
+++ app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreenViewModel.kt
@@ -4,9 +4,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kriptikz.shadowdrivemobile.data.services.ListObjectsRequest
+import com.kriptikz.shadowdrivemobile.data.services.ShadowDriveApi
+import com.kriptikz.shadowdrivemobile.data.services.ShadowFileNames
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface DriveUiState {
+    data class Success(val fileNames: List<String>) : DriveUiState
+    object Error : DriveUiState
+    object Loading : DriveUiState
+}
 
 class DriveScreenViewModel : ViewModel() {
-    var driveUiState: String by mutableStateOf("")
+    var driveUiState: DriveUiState by mutableStateOf(DriveUiState.Loading)
         private set
 
 
@@ -15,6 +27,15 @@ class DriveScreenViewModel : ViewModel() {
     }
 
     fun getDriveFiles() {
-        driveUiState = "Set the api response here!"
+//        driveUiState = "Set the api response here!"
+        viewModelScope.launch {
+            driveUiState = try {
+                val publicKey = "t5Cp1F6VcoeXxqNC7TrmYCJofT9U7iEPbziY252tPnX"
+                val result = ShadowDriveApi.retrofitService.listObjects(ListObjectsRequest(storageAccount = publicKey))
+                DriveUiState.Success(result.keys)
+            } catch (e: IOException) {
+                DriveUiState.Error
+            }
+        }
     }
 }
