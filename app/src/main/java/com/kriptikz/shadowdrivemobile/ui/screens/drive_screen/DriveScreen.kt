b3a0f59d3576ae8450581a78ec8050b825e327e5
Kriptikz <Kriptikz@users.noreply.github.com>
2022-11-27 13:27:53 -0500

    create a photo grid for DriveScreen to display all loaded photos

--- app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreen.kt
+++ app/src/main/java/com/kriptikz/shadowdrivemobile/ui/screens/drive_screen/DriveScreen.kt
@@ -1,17 +1,23 @@
 package com.kriptikz.shadowdrivemobile.ui.screens.drive_screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.kriptikz.shadowdrivemobile.ui.screens.SDMScaffold
 import com.kriptikz.shadowdrivemobile.ui.theme.ShadowDriveMobileTheme
 import com.kriptikz.shadowdrivemobile.R
@@ -21,10 +27,10 @@ fun DriveScreen(
     driveUiState: DriveUiState,
     modifier: Modifier = Modifier
 ) {
-    SDMScaffold(title = "Drives") {
+    SDMScaffold(title = "Drive") {
         when (driveUiState) {
             is DriveUiState.Loading -> LoadingScreen(modifier)
-            is DriveUiState.Success -> ResultScreen(fileNames = driveUiState.fileNames, modifier = modifier)
+            is DriveUiState.Success -> PhotosGridScreen(photoUrls = driveUiState.fileNames, modifier = modifier)
             is DriveUiState.Error -> ErrorScreen(modifier)
         }
     }
@@ -68,7 +74,52 @@ fun ResultScreen(fileNames: List<String>, modifier: Modifier = Modifier) {
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
     ) {
-      Text(text = "Found ${fileNames.size} files")
+//      Text(text = "Found ${fileNames.size} files")
+      Text(text = "${fileNames[0]}")
     }
 
+}
+
+@Composable
+fun DrivePhotoCard(imageUrl: String, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .aspectRatio(1f),
+        elevation = 8.dp,
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = "description",
+            error = painterResource(id = R.drawable.ic_broken_image),
+            placeholder = painterResource(id = R.drawable.loading_img),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun PhotosGridScreen(photoUrls: List<String>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(photoUrls.size) { index ->
+            if (photoUrls[index].endsWith(".jpg")) {
+                Column {
+                    Text(
+                        fontSize = 12.sp,
+                        text = photoUrls[index].split("/")[4],
+                        modifier = Modifier.height(16.dp).padding(start = 10.dp)
+                    )
+                    DrivePhotoCard(photoUrls[index])
+                }
+            }
+        }
+    }
 }
